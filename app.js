d4ab52b1f829d827386dfad7831651e91bb925c5
arulabdulaziz <rulz.matrixs@gmail.com>
2020-11-27 01:07:10 +0700

    debug

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const port = 3000;
 const router =require('./routes/index');
 const errorHandler = require('./middleware/errorHandler');
-
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json());
